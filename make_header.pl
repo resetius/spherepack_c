ec7b62121a8dc9b238a1ee017dd59d0934b4cafc
Alexey Ozeritsky <aozeritsky@gmail.com>
2010-02-09 11:44:08 +0300

    make header

---
+++ make_header.pl
@@ -0,0 +1,34 @@
+my $text = "";
+
+while ($name = glob("*.c")) {
+	open FILE, '<', $name;
+	while (<FILE>) {
+		$text .= $_;
+	}
+	close FILE;
+}
+
+my @funcs = $text =~ m/^(\/\* Subroutine \*\/.*?\))/gms;
+
+print "
+#ifndef SPHEREPACK_H
+#define SPHEREPACK_H
+
+#ifdef __cplusplus
+extern \"C\";
+#endif
+";
+
+foreach my $i (@funcs) {
+	print "$i;\n\n";
+}
+
+print "
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*SPHEREPACK_H*/
+";
+
